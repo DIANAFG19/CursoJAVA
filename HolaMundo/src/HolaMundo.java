17ab66993c6e0a9c9c1e1c0d446ce4d5a116b1e1
Diana <diana1310fg@gmail.com>
2022-06-29 12:35:56 -0500

    Variables

--- HolaMundo/src/HolaMundo.java
+++ HolaMundo/src/HolaMundo.java
@@ -1,2 +1,28 @@
+import java.util.Locale;
+
 public class HolaMundo {
+    public static void main(String[] args) {
+
+        //Variable tipo String.
+        String saludar = "Mi nombre es Diana Laura";
+        System.out.println(saludar);
+        //Variable tipo String convertida a mayúsculas.
+        System.out.println("saludar.toUpperCase() = " + saludar.toUpperCase());
+        //Variable de tipo int.
+        int numero = 10;
+        System.out.println("numero = " + numero);
+        //Variable tipo boolean.
+        boolean valor = true;
+
+        if (valor){
+            System.out.println("valor es verdadero");
+        }
+        else {
+            System.out.println("valor es falso");
+        }
+
+        //Variable de tipo asignada por su valor.
+        var numero5 = 35;
+
+    }
 }
