bf0bd76dbbe9cb90f9a4365161d432de19731c27
Diana <diana1310fg@gmail.com>
2022-07-02 00:07:08 -0500

    Conversion de primitivos a cadenas.

--- HolaMundo/src/ConversionDeTipos.java
+++ HolaMundo/src/ConversionDeTipos.java
@@ -6,20 +6,30 @@ public class ConversionDeTipos {
         int numeroInt = Integer.parseInt(numeroStr);
         System.out.println("numeroInt = " + numeroInt);
 
-        //Convertir String a byte
-        byte numeroByte = Byte.parseByte(numeroStr);
-        System.out.println("numeroByte = " + numeroByte);
+        //Convertir int a String
+        int numeroInt2 = 30;
+        String numeroStr2 = Integer.toString(numeroInt2);
+        System.out.println("numeroStr2 = " + numeroStr2);
 
         //Convertir String a double
         String realStr = "9847.545e-3";
         double realDouble = Double.parseDouble(realStr);
         System.out.println("realDouble = " + realDouble);
 
+        //Convertir double a String
+        double realDouble2 = 9847.545e-3;
+        String realStr2 = Double.toString(realDouble2);
+        System.out.println("realStr2 = " + realStr2);
+
         //Convertir String a boolean
         String logicoStr = "true";
         boolean logicoBoolean = Boolean.parseBoolean(logicoStr);
         System.out.println("logicoBoolean = " + logicoBoolean);
 
+        //Convertir boolean a String
+        boolean logicoBoolean2 = true;
+        String logicoStr2 = Boolean.toString(logicoBoolean2);
+        System.out.println("logicoStr2 = " + logicoStr2);
 
     }
 }
