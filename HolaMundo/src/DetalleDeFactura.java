63a4774cdf6fd09a0815eab65e89a79b3b8b30a5
Diana <diana1310fg@gmail.com>
2022-07-05 22:58:57 -0500

    Proyecto DetalleFactura (Scanner)

---
+++ HolaMundo/src/DetalleDeFactura.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class DetalleDeFactura {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Escribe el nombre de la factura o descripción: ");
+        String nombreFactura = scanner.nextLine();
+
+        System.out.println("Escribe el precio 1: ");
+        double precioUno = scanner.nextDouble();
+
+        System.out.println("Escribe el precio 2: ");
+        double precioDos = scanner.nextDouble();
+
+        double totalBruto = precioUno + precioDos;
+        double impuesto = totalBruto * 0.19;
+        double totalConImpuesto = totalBruto + impuesto;
+
+        System.out.println("La factura " + nombreFactura + " tiene un total bruto de " + totalBruto + ", con un impuesto de "
+                + impuesto + " y el monto después de impuesto es de " + totalConImpuesto);
+    }
+}
