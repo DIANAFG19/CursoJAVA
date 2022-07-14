396112f22b1ae47479d9cbfd885e29c6978c375f
Diana <diana1310fg@gmail.com>
2022-07-13 23:26:34 -0500

    Concatenación String.

--- HolaMundo/src/EjemploString.java
+++ HolaMundo/src/EjemploString.java
@@ -1,5 +1,7 @@
 public class EjemploString {
     public static void main(String[] args) {
+
+        //Creación de objeto.
         String curso = "Programación Java";
         String curso2 = new String("Programación Java");
 
@@ -8,5 +10,22 @@ public class EjemploString {
 
         boolean esIgual2 = curso.equals(curso2);
         System.out.println("esIgual2 = " + esIgual2);
+
+        //Concatenación.
+        String profesor = "Diana Laura Flores Garcia";
+        String detalle = curso + " con el instructor " + profesor;
+        System.out.println(detalle);
+
+        int numeroA = 10;
+        int numeroB = 5;
+        System.out.println(detalle + numeroA + numeroB);
+        System.out.println(detalle + (numeroA + numeroB));
+        System.out.println(numeroA + numeroB + detalle);
+
+        String detalle2 = curso.concat(" con " + profesor);
+        String detalle3 = curso.concat(" con ".concat(profesor));
+        System.out.println("detalle2 = " + detalle2);
+        System.out.println("detalle3 = " + detalle3);
+
     }
 }
