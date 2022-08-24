e6e85b8807759ff2f39f1eccfaba3ca3506fa94d
Diana <diana1310fg@gmail.com>
2022-08-24 18:41:45 -0500

    LOGIN usando Operadores Lógicos.

---
+++ HolaMundo/src/Login.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Login {
+    public static void main(String[] args) {
+        String user = "Diana";
+        String pswd = "Diana1234";
+
+        String user2 = "Jesus";
+        String pswd2 = "Jesus1234";
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Ingrese el usuario: ");
+        String usuario = scanner.next();
+
+        System.out.println("Ingrese la contraseña: ");
+        String contrasena = scanner.next();
+
+        boolean esAutenticado = false;
+
+        if (user.equals(usuario) && pswd.equals(contrasena) || user2.equals(usuario) && pswd2.equals(contrasena)) {
+            esAutenticado = true;
+        }
+
+        if (esAutenticado) {
+            System.out.println("¡¡BIENVENIDO USUARIO ".concat(usuario.toUpperCase()).concat("!!"));
+        }
+        else {
+            System.out.println("¡¡USUARIO Y/O CONTRASEÑA INCORRECTA!!");
+        }
+
+    }
+}
